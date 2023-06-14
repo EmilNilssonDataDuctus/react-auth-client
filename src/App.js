e36e2f407a9a81937fd835fc39da5468db6ebbba
Emil Nilsson <emil.nilsson@dataductus.se>
2023-06-14 17:00:20 +0200

    Add silent authentication for App.js for before the access token expires

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import axios from "axios";
+import { useCallback, useEffect } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "./components/Login/Login";
 import MyComponent from "./components/MyComponent/MyComponent";
 import Signup from "./components/Signup/Signup";
+import { useAuth } from "./contexts/auth-context";
 
 function App() {
+  const { login, logout, isAuthenticated, expiresAt } = useAuth();
+
+  const refreshAccessToken = useCallback(async () => {
+    try {
+      const response = await axios.post(
+        "/api/auth/refresh",
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+
+      const { user, accessToken, expiresAt } = response.data;
+
+      if (response.status === 204) {
+        logout();
+      } else {
+        login(user, accessToken, expiresAt);
+      }
+    } catch (error) {
+      logout();
+    }
+  }, [login, logout]);
+
+  useEffect(() => {
+    refreshAccessToken();
+    console.log("run once");
+  }, [refreshAccessToken]);
+
+  useEffect(() => {
+    let refreshAccessTokenTimerId;
+
+    if (isAuthenticated) {
+      refreshAccessTokenTimerId = setTimeout(() => {
+        refreshAccessToken();
+      }, new Date(expiresAt).getTime() - Date.now() - 10 * 1000);
+    }
+
+    return () => {
+      if (isAuthenticated && refreshAccessTokenTimerId) {
+        clearTimeout(refreshAccessTokenTimerId);
+      }
+    };
+  }, [expiresAt, isAuthenticated, refreshAccessToken]);
+
   const router = createBrowserRouter([
     { path: "/", element: <h1>Hello World!</h1> },
     { path: "sign-up", element: <Signup /> },
