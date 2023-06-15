1e2976c1bb7b3ae774a0ed0a8c2e727a56f11bd4
Emil Nilsson <emil.nilsson@dataductus.se>
2023-06-15 15:03:53 +0200

    Add users component

---
+++ src/components/Users/Users.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useAuth } from "../../contexts/auth-context";
+import User from "./User/User.js";
+import styles from "./Users.module.scss";
+
+const Users = () => {
+  const { token } = useAuth();
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    axios
+      .get("/api/users/list", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        setUsers(res.data.data);
+      })
+      .catch((error) => {
+        console.log("Something went wrong.", error);
+      });
+  }, [token]);
+
+  return (
+    <div className={styles.container}>
+      {users.map((user) => (
+        <div key={user.id} className={styles.userContainer}>
+          <User user={user} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Users;
